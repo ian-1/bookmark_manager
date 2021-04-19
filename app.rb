03fc4fbca855ea6c355bf73891b061b62c4c4dbd
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-19 15:03:03 +0100

    wip step 3

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
-
+require 'sinatra/reloader'
 
 class BookmarkManager < Sinatra::Base
   
+  configure :development do
+    register Sinatra::Reloader
+  end
+  
   get '/' do
     erb :index
+  end
+
+  get '/bookmarks' do
+    erb :bookmarks
   end
 
   run! if app_file == $0
